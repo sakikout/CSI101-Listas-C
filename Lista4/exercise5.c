41c40a787dca58c91f20f960f2b64274551d15dd
Bela D <86171066+sakikout@users.noreply.github.com>
2022-09-17 18:45:17 -0300

    Operators updated
    i literally forgot to use "==" instead of "=" in the "If" statements. thanks Tom :D

--- Lista4/exercise5.c
+++ Lista4/exercise5.c
@@ -2,17 +2,17 @@
 
 void Estacao(int dia, int mes){
 
-   if (mes = 12 && dia > 21 || mes = 1 && dia < 31|| mes = 2 && dia < 30|| mes = 3 && dia < 21){
-    printf("\nData: %d, %d\nEstacao: Verao", dia, mes);
+   if (mes == 12 && dia > 21 || mes == 1 && dia < 31|| mes == 2 && dia < 30|| mes == 3 && dia < 21){
+    printf("\nData: %d, %d\nEstacao: Verao\n\n", dia, mes);
 
-   } else if (mes = 3 && dia > 21 || mes = 4 && dia < 30 || mes = 5 && dia < 30 || mes = 6 && dia < 21){
-    printf("\nData: %d, %d\nEstacao: Outono", dia, mes);
+   } else if (mes == 3 && dia > 21 || mes == 4 && dia < 30 || mes == 5 && dia < 30 || mes == 6 && dia < 21){
+    printf("\nData: %d, %d\nEstacao: Outono\n\n", dia, mes);
 
-   } else if (mes = 6 && dia > 21 || mes = 7 && dia < 30 || mes = 8 && dia < 31 || mes = 9 && dia < 23){
-    printf("\nData: %d, %d\nEstacao: Inverno", dia, mes);
+   } else if (mes == 6 && dia > 21 || mes == 7 && dia < 30 || mes == 8 && dia < 31 || mes == 9 && dia < 23){
+    printf("\nData: %d, %d\nEstacao: Inverno\n\n", dia, mes);
 
-   } else if (mes = 9 && dia > 23 || mes = 10 && dia < 30 || mes = 11 && dia < 30 || mes = 12 && dia < 21){
-    printf("\nData: %d, %d\nEstacao: Primavera", dia, mes);
+   } else if (mes == 9 && dia > 23 || mes == 10 && dia < 30 || mes == 11 && dia < 30 || mes == 12 && dia < 21){
+    printf("\nData: %d, %d\nEstacao: Primavera\n\n", dia, mes);
 
    } else {
     printf("\nInvalido.\n\n");
@@ -25,11 +25,11 @@ int main (void){
     int dias, meses;
 
     do{
-        printf("\nInsira o dia e mes para encontrar a estacao correspondente:");
-        printf("Dia: ");
+        printf("Insira o dia e mes para encontrar a estacao correspondente:");
+        printf("\nDia: ");
         scanf("%d", &dias);
 
-        printf("Mes: ");
+        printf("\nMes: ");
         scanf("%d", &meses);
 
         Estacao(dias, meses);
